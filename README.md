da5023c9124877ce61c9297272e1299e0708fe63
Pascal Brun <brunpasc@gmail.com>
2021-07-15 07:52:42 +0200

    chore(app): renaming

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ConsumeApi
+# ubinetic Oracles Dashboard
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'consume-api'`, () => {
+  it(`should have as title 'ubinetic-oracles-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('consume-api');
+    expect(app.title).toEqual('ubinetic-oracles-dashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('consume-api app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'ubinetic-oracles-dashboard app is running!'
+    );
   });
 });
